e8daabe9d0aa0594eb07b38c1873f3d0cfa9178a
michele-pupo <mikypupo@hotmail.it>
2024-02-14 14:33:35 +0100

    printed in console array of objects

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-Consegna:
+// Consegna:
 //  Dato un array di oggetti letterali con:
 //  - url dell’immagine
 //  - titolo
@@ -23,91 +23,54 @@ Consegna:
 // BONUS 3:
 // - Aggiungere bottoni di start / stop e di inversione del meccanismo di autoplay.
 
-const images = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"];
-console.log(images);
-
-
-// bersagliamo lo slider
-const sliderElement = document.getElementById("slider");
-
-
-// tramite un ciclo for prendiamo ogni indirizzo delle immagini dall'array
-for (let i = 0; i < images.length; i++) {
-
-    sliderElement.innerHTML += `<img src="./img/0${i + 1}.webp" alt="immagine ${i + 1}">`;
-
-    // per ognuno di essi andremo a creare un elemento img dentro lo slider
-
-}
-
-document.querySelector("#slider img:nth-of-type(1)").className = "active";
-
-/*
--  salvo un contatore della slide
--  QUANDO premo la freccia SU
-    - prendo l'immagine attuale e le rimuovo la classe "active"  
-    - aumento il contatore di 1
-    - prendo l'immagine con il nuovo contatore e le aggiungo la classe "active"
-*/
-
-
-// -  salvo un contatore della slide
-let slideNumber = 1;
-
-// -  QUANDO premo la freccia SU
-document.querySelector("#up-arrow").addEventListener("click", function() {
-
-
-    if (slideNumber < images.length) {
-
-        // - prendo l'immagine attuale e le rimuovo la classe "active"  
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.remove("active");
-
-        // - aumento il contatore di 1
-        slideNumber++;
-
-        // - prendo l'immagine con il nuovo contatore e le aggiungo la classe "active"
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.add("active");
-
-        console.log(slideNumber);
-
-    } else {
-
-        // - prendo l'immagine attuale e le rimuovo la classe "active"  
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.remove("active");
-
-        // resetto la variabile che mi conta l'immagine a cui sono arrivato
-        slideNumber = 1;
-
-        // - prendo l'immagine con il nuovo contatore e le aggiungo la classe "active"
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.add("active");
+// creiamo un array di oggetti con le informazioni fornite
+const imagesArray = [
+    {
+    "image": 'img/01.webp',
+    "title": 'Marvel\'s Spiderman Miles Morale',
+    "text" : 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.'
+    }, 
+    {
+    "image": 'img/02.webp',
+    "title": 'Ratchet & Clank: Rift Apart',
+    "text" : 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.'
+    }, 
+    {
+    "image": 'img/03.webp',
+    "title": 'Fortnite',
+    "text" : "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos."
+    }, 
+    {
+    "image": 'img/04.webp',
+    "title": 'Stray',
+    "text" : 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city'
+    }, 
+    {
+    "image": 'img/05.webp',
+    "title": "Marvel's Avengers",
+    "text" : 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.'
     }
-});
+];
 
+// stampiamo in console l'array di oggetti
+console.log(imagesArray);
 
-document.querySelector("#down-arrow").addEventListener("click", function() {
+// stampiamo in pagina le informazioni fornite
+const listElement = document.getElementById("slide-photo");
 
-    if (slideNumber > 1) {
-        // - prendo l'immagine attuale e le rimuovo la classe "active"  
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.remove("active");
+// ciclo for per iterare su ogni elemtno dell'array (oggetto)
+for (let i = 0; i < imagesArray.length; i++){
 
-        // - diminuisco il contatore di 1
-        slideNumber--;
+    const actualPhoto = imagesArray[i];
 
-        // - prendo l'immagine con il nuovo contatore e le aggiungo la classe "active"
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.add("active");
+    let textOnPage = "";
 
-        console.log(slideNumber);
+    // ciclo "for-in" per ciclare su ogni chiave dell'oggetto
+    for (let key in actualPhoto){
 
-    } else {
+        // stampiamo in console le informazioni fornite tramite oggetti
+        console.log(`${key}: ${actualPhoto[key]}`);
 
-        // - prendo l'immagine attuale e le rimuovo la classe "active"  
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.remove("active");
-
-        // - metto il valore di slideNumebr = alla posizione dell'ultima immagine
-        slideNumber = images.length;
-
-        // - prendo l'immagine con il nuovo contatore e le aggiungo la classe "active"
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.add("active");
+        textOnPage += `${key}: ${actualPhoto[key]}`;
     }
-});
+}    
